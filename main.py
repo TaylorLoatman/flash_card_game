316c424bfc4b554c1cdc479c468789c7474ff83f
Taylor Loatman <taylor.loatman@icloud.com>
2021-09-28 17:48:15 -0400

    Add UI set-up to application

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+from tkinter import *
+
+
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Flashcard App")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
+flash_img = PhotoImage(file='images/card_front.png')
+canvas.create_image(400, 300, image=flash_img)
+title_text = canvas.create_text(400, 150, text="Title", fill="black", font=("Ariel", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="Word", fill="black", font=("Arial", 60, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+
+correct_img = PhotoImage(file='images/right.png')
+correct_button = Button(image=correct_img, highlightthickness=0)
+correct_button.grid(column=0, row=1)
+
+wrong_img = PhotoImage(file='images/wrong.png')
+wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button.grid(column=1, row=1)
+
+
+window.mainloop()
